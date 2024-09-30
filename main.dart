c61482939dcbf3099cf69b8aba1612263e16b075
ManagerEx <achmadqaafi1709@gmail.com>
2024-09-30 14:00:42 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     home : Scaffold(
+      body: Container(
+       padding: const EdgeInsets.all(20),
+       height: 150,
+       child: ListView(
+          scrollDirection: Axis.horizontal,
+          
+          children : [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text("Hello Tes"),
+            ),
+          ]
+          ),
+        ),
+      ),
+    );
+  } 
+} 
